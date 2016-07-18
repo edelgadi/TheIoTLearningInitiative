1e14e2d0974ce0329fd1dc31ec8b56ec441c3196
Edgar Delgadillo <edgar.delgadillo@intel.com>
2016-07-18 23:07:45 +0000

    i2c:driver:th02
    Convertion of temperature and humidity was corrected using the data
    sheet info
    
    Signed-off-by: Edgar Delgadillo <edgar.delgadillo@intel.com>

--- InternetOfThings101/TH02.py
+++ InternetOfThings101/TH02.py
@@ -10,19 +10,26 @@ class TH02:
 
     def readTemperature(self):
         self.x.writeReg(0x03,0x11)
-        while self.x.readReg(0x00) and 0x01:
+        while self.x.readReg(0x00) & 0x01:
              pass
         a = self.x.readBytesReg(0x01,2)
-        return ((a[0]<<8)|a[1])/128.0-50.0
+        #Accord with data sheet the last 2 bits are trash
+        b = ((a[0]<<8)|a[1])>>2
+        return (b/32.0)-50.0
 
     def readHumidity(self):
         self.x.writeReg(0x03,0x01)
-        while self.x.readReg(0x00) and 0x01:
+        while self.x.readReg(0x00) & 0x01:
              pass
         a = self.x.readBytesReg(0x01,2)
-        return ((a[0]<<8)|a[1])/512.0-24.0
+        #Accord with data sheet the last 4 bits are trash
+        b = ((a[0]<<8)|a[1])>>4
+        return (b/16.0)-24.0
 
-#a=TH02(1)
-#while True:
-#     print a.readTemperature()
-#     print a.readHumidity()
+'''
+a=TH02(1)
+while True:
+    print "T %d" %a.readTemperature()
+    print "H %d" %a.readHumidity()
+    time.sleep(1)
+'''
