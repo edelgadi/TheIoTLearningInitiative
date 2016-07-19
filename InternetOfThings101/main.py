49bf0893d67e89fd10fa84518ec8974a465a883c
Edgar Delgadillo <edgar.delgadillo@intel.com>
2016-07-19 08:47:36 +0000

    Internet of Things 101: 5.5.1 API: Project: Challenge
    Added support to send information via mosquitto and dweet, also read
    info directly from two sensors (Temperature/humidity & Light)
    
    Signed-off-by: Edgar Delgadillo <edgar.delgadillo@intel.com>

--- InternetOfThings101/main.py
+++ InternetOfThings101/main.py
@@ -3,13 +3,33 @@
 import paho.mqtt.client as paho
 import psutil
 import pywapi
+import dweepy
 import signal
 import sys
 import time
+import pyupm_grove as grove
 
+from TH02 import *
+from flask import Flask
+from flask_restful import Api, Resource
 from threading import Thread
 
 DeviceID = "90b68610b05b"
+
+#RESTFull support class
+app = Flask(__name__)
+api = Api(app)
+
+class DataSensorRestApi(Resource):
+    def get(self):
+        data = 'Temperature: %d *C     '%sensTH.readTemperature()
+        data = data + 'Humidity: %d%%     '%sensTH.readHumidity()
+        data = data + 'Light: %d Lux'%sensL.value()
+        return data
+#End RESTFull support
+
+sensTH = TH02(1)
+sensL = grove.GroveLight(0)
 
 def functionApiWeather():
     data = pywapi.get_weather_from_weather_com('MXJO0043', 'metric')
@@ -31,14 +51,19 @@ def functionDataActuatorMqttSubscribe():
     mqttclient = paho.Client()
     mqttclient.on_message = functionDataActuatorMqttOnMessage
     mqttclient.connect("test.mosquitto.org", 1883, 60)
-    mqttclient.subscribe("IoT101/DataActuator", 0)
+    mqttclient.subscribe("IoT101/"+DeviceID+"/DataActuator", 0)
     while mqttclient.loop() == 0:
         pass
 
 def functionDataSensor():
-    netdata = psutil.net_io_counters()
-    data = netdata.packets_sent + netdata.packets_recv
-    return data
+#    netdata = psutil.net_io_counters()
+#    data = netdata.packets_sent + netdata.packets_recv
+    temp = sensTH.readTemperature()
+    hum = sensTH.readHumidity()
+    lig = sensL.value()
+    dweepy.dweet_for('IoT'+DeviceID, {'Temp':str(temp), \
+'Hum':str(hum),'Lig':str(lig)})
+    print dweepy.get_latest_dweet_for('IoT'+DeviceID)
 
 def functionDataSensorMqttOnPublish(mosq, obj, msg):
     print "Data Sensor Mqtt Published!"
@@ -49,7 +74,7 @@ def functionDataSensorMqttPublish():
     mqttclient.connect("test.mosquitto.org", 1883, 60)
     while True:
         data = functionDataSensor()
-        topic = "IoT101/DataSensor"
+        topic = "IoT101/"+DeviceID+"DataSensor"
         mqttclient.publish(topic, data)
         time.sleep(1)
 
@@ -66,10 +91,13 @@ if __name__ == '__main__':
     threadmqttsubscribe = Thread(target=functionDataActuatorMqttSubscribe)
     threadmqttsubscribe.start()
 
+    api.add_resource(DataSensorRestApi, '/sensor')
+    app.run(host='0.0.0.0', debug=True)
+
     while True:
         print "Hello Internet of Things 101"
         print "Data Sensor: %s " % functionDataSensor()
         print "API Weather: %s " % functionApiWeather()
-        time.sleep(5)
+        time.sleep(2)
 
 # End of File
